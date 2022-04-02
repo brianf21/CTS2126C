c17b3ffb0724640ab2a7336c6a298c89612a9661
brianf21 <brian.faris@gmail.com>
2022-04-02 00:26:34 -0400

    Update hackazon.sh

--- hackazon.sh
+++ hackazon.sh
@@ -11,5 +11,7 @@ systemctl enable docker
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/Newlode/hackazon/
+reboot
+
 # cd hackazon/
-# docker-compose up
+# docker-compose up&
